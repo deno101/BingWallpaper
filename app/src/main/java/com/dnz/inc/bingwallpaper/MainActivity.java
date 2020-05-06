81f1871b39c7d0e36afba15c8e624e8dec3572d4
deno101 <dennizwarui@gmail.com>
2020-05-06 11:11:44 +0300

    add app bar to main fragment

--- app/src/main/java/com/dnz/inc/bingwallpaper/MainActivity.java
+++ app/src/main/java/com/dnz/inc/bingwallpaper/MainActivity.java
@@ -1,14 +1,25 @@
 package com.dnz.inc.bingwallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Fragment mainFragment, displayImageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainFragment = new MainFragment();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, mainFragment)
+                    .commit();
+        }
     }
 }
